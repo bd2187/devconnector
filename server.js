4a3f670c64cf976a66f407a36aa1767b1da3430c
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-05-15 17:27:32 -0700

    Require mongoose and connect to mongo database

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
 
+// DATABASE CONFIG & CONNECTION
+const { mongoURI: db } = require("./config/keys");
+mongoose
+  .connect(db)
+  .then(() => console.log("Connected to DB"))
+  .catch(() => console.log("\x1b[33m%s\x1b[0m", "Connection err"));
+
 app.get("/", function(req, res) {
-  res.end("hello");
+  res.send("hello!");
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Now listening to port ${port}`);
 });
