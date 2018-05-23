57bc554ac1565e8231cbdcd0874b8e9b1e9418e4
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-05-22 22:59:16 -0700

    Create post route for /login. Find email in DB and compare passwords

--- routes/api/users.js
+++ routes/api/users.js
@@ -68,4 +68,32 @@ router.post("/register", function(req, res) {
   });
 });
 
+/*
+  @route  GET api/users/login
+  @desc   Login user / Returning JWT Token
+  @access Public
+*/
+
+router.post("/login", function(req, res) {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  // Find user by email
+  User.findOne({ email }).then(function(user) {
+    // Check for user
+    if (!user) {
+      return res.status(404).json({ email: "User not found" });
+    }
+
+    // Check password
+    bcrypt.compare(password, user.password).then(function(isMatch) {
+      if (isMatch) {
+        res.json({ message: "success" });
+      } else {
+        return res.status(400).json({ password: "Password incorrect" });
+      }
+    });
+  });
+});
+
 module.exports = router;
