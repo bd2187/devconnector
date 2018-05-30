a2892fc17441b4f134c9d9a63e9e88cc9edd3530
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-05-30 08:35:26 -0700

    Create profile route. Use passport.authenticate to ensure that it's a private route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,16 +1,35 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+// Load User and Profile models
+const User = require("../../models/User");
+const Profile = require("../../models/Profile");
 
 /*
-  @route  GET api/profile/
+  @route  GET api/test
   @desc   
   @access Public
 */
-router.get("/", function(req, res) {
+router.get("/test", function(req, res) {
   res.json({
     status: "success",
     message: "profile api"
   });
 });
+
+/*
+  @route  GET api/profile/
+  @desc   
+  @access Private
+*/
+router.get(
+  "/profile",
+  passport.authenticate("jwt", { session: false }),
+  function(req, res) {
+    // Find user
+  }
+);
 
 module.exports = router;
