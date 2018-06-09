d9bd97260e3e9439f72ee457de36e7af016c3dfb
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-06-09 10:31:40 -0700

    Create post route for /api/posts/. Implement post validation and store fields in Post object. Save object to DB

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,5 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport");
+
+// Load User and Posts models
+const User = require("../../models/User");
+const Posts = require("../../models/Posts");
+
+// Validation
+const validatePostInput = require("../../validation/post");
 
 /*
   @route  GET api/posts/
@@ -10,6 +19,35 @@ router.get("/", function(req, res) {
   res.json({
     status: "success",
     message: "posts api"
+  });
+});
+
+/*
+  @route  POST api/posts/
+  @desc   Create post
+  @access Private
+*/
+
+router.post("/", passport.authenticate("jwt", { session: false }), function(
+  req,
+  res
+) {
+  const { errors, isValid } = validatePostInput(req.body);
+
+  // Check validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
+  const newPost = new Post({
+    text: req.body.text,
+    name: req.body.name,
+    avatar: req.body.avatar,
+    user: req.user.id
+  });
+
+  newPost.save().then(function(post) {
+    return res.json(post);
   });
 });
 
