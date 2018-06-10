09314c76486a4316ecdb19cd897cb6d3c13bcffe
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-06-10 12:25:08 -0700

    Load profile model. Implement DELETE route for posts.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const passport = require("passport");
 
-// Load User and Posts models
+// Load models
 const User = require("../../models/User");
 const Posts = require("../../models/Posts");
+const Profile = require("../../models/Profile");
 
 // Validation
 const validatePostInput = require("../../validation/post");
@@ -36,7 +37,6 @@ router.get("/", function(req, res) {
   @access Public
 */
 router.get("/:id", function(req, res) {
-  console.log(req.params.id);
   Posts.findById(req.params.id, function(err, post) {
     return err
       ? res.status(404).json({ status: "failed", err })
@@ -71,5 +71,35 @@ router.post("/", passport.authenticate("jwt", { session: false }), function(
     return res.json(post);
   });
 });
+
+/*
+  @route  DELETE api/posts/:id
+  @desc   Delete post
+  @access Private
+*/
+
+router.delete(
+  "/:id",
+  passport.authenticate("jwt", { session: false }),
+  function(req, res) {
+    Profile.findOne({ user: req.user.id })
+      .then(function(profile) {
+        return Posts.findById(req.params.id);
+      })
+      .then(function(post) {
+        // If user attempts to delete a message that doesn't match user's id, send them a non authorization message
+        if (post.user.toString() !== req.user.id) {
+          return res
+            .status(401)
+            .json({ status: "fail", notauthorized: "user not authorized" });
+        }
+
+        return post
+          .remove()
+          .then(() => res.json({ status: "success", msg: "deleted post" }));
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
 
 module.exports = router;
