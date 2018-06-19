bb5c61ab8eeafc9199c07df5ea93cbcdbef55454
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-06-18 23:12:00 -0700

    Remove components that use routing. Place them in routes.js. Import routes constant from routes.js and use the constant in BrowserRouter component

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,10 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
 
-import Landing from "./components/layout/Landing";
-import Login from "./components/auth/Login";
-import Register from "./components/auth/Register";
+import routes from "./components/routes";
 
 import "./App.css";
 
@@ -16,13 +14,7 @@ class App extends Component {
       <BrowserRouter>
         <div className="App">
           <Navbar />
-          <Switch>
-            <Route exact path="/" component={Landing} />
-            <div className="container">
-              <Route path="/register" component={Register} />
-              <Route path="/login" component={Login} />
-            </div>
-          </Switch>
+          {routes}
           <Footer />
         </div>
       </BrowserRouter>
