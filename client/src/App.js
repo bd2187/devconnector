984532bac13195365ca290639fe0a5105d7cdca0
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-06-18 20:51:25 -0700

    Set up react-router-dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,31 @@
 import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
 import Navbar from "./components/layout/Navbar";
-import Landing from "./components/layout/Landing";
 import Footer from "./components/layout/Footer";
+
+import Landing from "./components/layout/Landing";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
+
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-        <Footer />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={Landing} />
+            <div className="container">
+              <Route path="/register" component={Register} />
+              <Route path="/login" component={Login} />
+            </div>
+          </Switch>
+          <Footer />
+        </div>
+      </BrowserRouter>
     );
   }
 }
