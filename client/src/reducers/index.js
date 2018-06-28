0e9195543663ca6c1a316401f53513ee5346f112
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-06-27 21:52:29 -0700

    Import errorsReducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,10 @@
 import { combineReducers } from "redux";
 import authReducer from "./authReducer";
+import errorsReducer from "./errorsReducer";
 
-const rootReducer = combineReducers({ auth: authReducer });
+const rootReducer = combineReducers({
+  auth: authReducer,
+  errors: errorsReducer
+});
 
 export default rootReducer;
