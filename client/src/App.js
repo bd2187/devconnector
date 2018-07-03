59ae9a395ad0d880c5cde9ca061828de853eb508
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-07-03 14:09:19 -0700

    Add logic which checks for user's jwtToken in localStorage. If it exists, set the token as the authorization header, decode the token and dispatch the setCurrentUser action creator with the decoded jwt token.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
 import { BrowserRouter } from "react-router-dom";
+
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
+import { setCurrentUser } from "./actions/authActions";
+
 import { Provider } from "react-redux";
 import store from "./store";
 
@@ -9,6 +14,19 @@ import Footer from "./components/layout/Footer";
 import routes from "./components/routes";
 
 import "./App.css";
+
+// Check for token
+
+if (localStorage.jwtToken) {
+  // Set Auth token header auth
+  setAuthToken(localStorage.jwtToken);
+
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(localStorage.jwtToken);
+
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+}
 
 class App extends Component {
   render() {
