7d3d93906bf68e0948ec23b0e201bbbfdb0e4a96
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-07-05 19:28:29 -0700

    Import the clearCurrentProfile action creator. If decoded.exp is less than currentTime, dispatch the clearCurrentProfile action creator

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import jwt_decode from "jwt-decode";
 import setAuthToken from "./utils/setAuthToken";
 import { setCurrentUser, logoutUser } from "./actions/authActions";
+import { clearCurrentProfile } from "./actions/profileActions";
 
 import { Provider } from "react-redux";
 import store from "./store";
@@ -33,6 +34,9 @@ if (localStorage.jwtToken) {
   if (decoded.exp < currentTime) {
     // Logout user
     store.dispatch(logoutUser());
+
+    // Clear current profile
+    store.dispatch(clearCurrentProfile());
 
     // Clear current profile
     // Redirect to login
