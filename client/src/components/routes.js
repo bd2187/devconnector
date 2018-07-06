4cd956b0361a1b2bc9cd4acad02ea9c8875278a5
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-07-05 19:32:51 -0700

    Create route for /dashboard using the Dashboard component

--- client/src/components/routes.js
+++ client/src/components/routes.js
@@ -4,6 +4,7 @@ import { Switch, Route } from "react-router-dom";
 import Landing from "./layout/Landing";
 import Register from "./auth/Register";
 import Login from "./auth/Login";
+import Dashboard from "./dashboard/Dashboard";
 
 const routes = (
   <Switch>
@@ -11,6 +12,7 @@ const routes = (
     <div className="container">
       <Route path="/register" component={Register} />
       <Route path="/login" component={Login} />
+      <Route path="/dashboard" component={Dashboard} />
     </div>
   </Switch>
 );
