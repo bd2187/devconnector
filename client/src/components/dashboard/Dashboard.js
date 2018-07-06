0db440fe666fe9604804b437b2d1a83197120d1e
Brandon Dionisio <brandon.dionisio.26@gmail.com>
2018-07-06 08:37:40 -0700

    Render dashboard UI. Pass auth and profile state to Dashboard component. Implement conditional rendering for loading and displaying a profile if a user has a profile

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,30 +1,69 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
 import { connect } from "react-redux";
 import { getCurrentProfile } from "../../actions/profileActions";
+
+import Spinner from "../common/Spinner";
 
 class Dashboard extends Component {
   componentDidMount() {
     this.props.getCurrentProfile();
   }
 
+  displayProfile(profile, user) {
+    if (Object.keys(profile).length > 0) {
+      return <h4>display profile</h4>;
+    } else {
+      return (
+        <div>
+          <p className="lead text-muted">Welcome {user.name}</p>
+          <p>You have not yet set up a profile. Please add some info.</p>
+          <Link to="/create-profile" className="btn-lg btn-info">
+            Create Profile
+          </Link>
+        </div>
+      );
+    }
+  }
+
   render() {
+    const { user } = this.props.auth;
+    const { profile, loading } = this.props.profile;
+
+    let dashboardContent =
+      profile === null || loading ? (
+        <Spinner />
+      ) : (
+        this.displayProfile(profile, user)
+      );
+
     return (
-      <div>
-        <h1>Dashboard</h1>
+      <div className="dashboard">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4">Dashboard</h1>
+              {dashboardContent}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
 
 Dashboard.propTypes = {
-  getCurrentProfile: PropTypes.func.isRequired
+  getCurrentProfile: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+  profile: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => {
   return {
-    profile: state.profile.profile,
-    loading: state.profile.loading
+    auth: state.auth,
+    profile: state.profile
   };
 };
 
